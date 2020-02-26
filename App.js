ca8e62c928ddd191b2fc19d9e36e00dd49404e12
esteban <jelicich.e@gmail.com>
2020-02-26 10:44:31 -0300

    course-

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'pepe', id: '2' },
+    { name: 'caca', id: '3' },
+    { name: 'asdsd', id: '4' },
+    { name: 'dsids', id: '5' },
+    { name: 'dsids', id: '6' },
+    { name: 'dsids', id: '7' },
+    { name: 'end', id: '8' },
+
+  ]);
+ 
+
+  const clickHandler = () => {
+    setName('chun li')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}/>
+
+      
+      {/* <ScrollView>
+      {people.map((item) => {
+        return(
+          <View key={item.key}> 
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        )
+      })}
+      </ScrollView> */}
     </View>
   );
 }
@@ -13,7 +47,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'red',
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
+  }
 });
